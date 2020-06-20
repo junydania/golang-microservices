374f9022323e2a5b1a93e8f1d048b8cc0b0c36cd
Dania <o.dania@icloud.com>
2020-06-20 17:02:31 +0200

    added graceful shutdown

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/junydania/golang-microservices
 
 go 1.14
+
+require sigs.k8s.io/kind v0.8.1 // indirect
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"net/http"
 	"log"
 	"os"
+	"os/signal"
 	"github.com/junydania/golang-microservices/handlers"
+	"time"
 )
+
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
@@ -15,5 +18,26 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe("127.0.0.1:9090", sm)
+	s := &http.Server{
+		Addr: "127.0.0.1:9090",
+		Handler: sm,
+		IdleTimeout: 	120 * time.Second,
+		ReadTimeout: 	1 * time.Second,
+		WriteTimeout: 	1 * time.Second,
+	}
+	go func(){
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatalf(err)
+		}
+	}
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate", "graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }

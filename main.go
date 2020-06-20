cdd971900661eebfe16c75580c8a9a30ce19f640
Dania <o.dania@icloud.com>
2020-06-20 15:49:51 +0200

    added handler

--- main.go
+++ main.go
@@ -5,20 +5,15 @@ import (
 	"log"
 	"io/ioutil"
 	"fmt"
+	"os"
+	"github.com:junydania/golang-microservices/handlers"
 )
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request){
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("Ooops"))
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
-		log.Println("Goodbye World")
-	})
-	http.ListenAndServe("127.0.0.1:9090", nil)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello()
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.ListenAndServe("127.0.0.1:9090", sm)
 }
